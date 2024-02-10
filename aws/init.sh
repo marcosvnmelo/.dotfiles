e349c1f887f72bc50f90878620f388fe9b39d0f3
Marcos Melo <suiciniv2016+github@outlook.com>
2024-02-10 17:06:33 -0300

    fix: delete correct aws files

--- aws/init.sh
+++ aws/init.sh
@@ -3,8 +3,8 @@
 echo 'Installing AWS CLI'
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+unzip awscliv2.zip -d awscli
+sudo ./awscli/install
 
 rm awscliv2.zip
-rm -rf aws
+rm -rf awscli
