c07cf20256839d80b6ad5f40dcab2064794280bc
Marcos Melo <suiciniv2016+github@outlook.com>
2024-02-10 17:07:44 -0300

    fix: delete lazygit installation files

--- nvim/init.sh
+++ nvim/init.sh
@@ -24,6 +24,8 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz
+rm lazygit
 
 pnpm i -g neovim
 pnpm i -g tree-sitter-cli
