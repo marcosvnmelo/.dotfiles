a423e56d3bc234217e1cc50eff7b6427832617c4
Marcos Melo <suiciniv2016+github@outlook.com>
2024-02-10 15:51:25 -0300

    feat: neovim dependencies

--- nvim/init.sh
+++ nvim/init.sh
@@ -13,3 +13,14 @@ sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 mkdir -p ~/.config/nvim
 
 git clone https://github.com/marcosvnmelo/nvim-config ~/.config/nvim
+
+# Install neovim dependencies
+
+sudo apt install -y gcc
+sudo apt install -y ripgrep
+sudo apt install -y fd-find
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
