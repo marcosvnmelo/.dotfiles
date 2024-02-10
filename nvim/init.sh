c3c701aaf75e2b5f35e3e582aada9d1a5446a5d7
Marcos Melo <suiciniv2016+github@outlook.com>
2024-02-10 17:35:55 -0300

    fix: add build-essential to nvim init

--- nvim/init.sh
+++ nvim/init.sh
@@ -19,6 +19,7 @@ git clone https://github.com/marcosvnmelo/nvim-config ~/.config/nvim
 sudo apt install -y gcc
 sudo apt install -y ripgrep
 sudo apt install -y fd-find
+sudo apt install -y build-essential
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -28,4 +29,4 @@ rm lazygit.tar.gz
 rm lazygit
 
 pnpm i -g neovim
-pnpm i -g tree-sitter-cli
+pnpm i -g tree-sitter
