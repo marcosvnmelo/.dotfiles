72edfefe2324a94315968fb65e1064f65a7dae35
Marcos Melo <suiciniv2016+github@outlook.com>
2024-02-13 19:01:47 -0300

    feat: tmux plugin manager

--- tmux/init.sh
+++ tmux/init.sh
@@ -2,6 +2,10 @@
 
 echo 'Installing tmux'
 
+# Clone Tmux Plugin Manager
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Create a symlink for the tmux configuration file
 
 ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
