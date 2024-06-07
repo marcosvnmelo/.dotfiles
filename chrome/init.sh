bb5e17158c4c2c34dafeab3af60260deb4d4f1a2
Marcos Melo <suiciniv2016+github@outlook.com>
2024-06-07 03:29:27 -0300

    fix(chrome): add accept flag to apt command

--- chrome/init.sh
+++ chrome/init.sh
@@ -6,6 +6,6 @@ echo '*****************************************'
 
 curl -L 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -o 'chrome.deb'
 
-sudo apt install --fix-missing ./chrome.deb
+sudo apt install --fix-missing ./chrome.deb -y
 
 rm chrome.deb
