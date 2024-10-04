72bdd04650f6a6406155e2443c4ed644cd965eae
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-03 22:06:56 -0300

    chore(lazygit): add installing message to script

--- lazygit/init.sh
+++ lazygit/init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+echo '******************************************'
+echo '*           Installing Lazygit           *'
+echo '******************************************'
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 mkdir tmp
