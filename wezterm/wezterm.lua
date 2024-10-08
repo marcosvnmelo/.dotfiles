9e47b077e12f8d38d67cdb318af1177365ef7081
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-07 22:04:51 -0300

    refactor(wezterm): increase font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,7 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "AdventureTime"
 
 config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 11
+config.font_size = 12
 
 config.window_background_opacity = 0.9
 config.enable_wayland = false
