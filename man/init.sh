cfc6e57f3761e50e4464bd771e20fad7405eb378
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-22 14:05:41 -0300

    feat: man module

---
+++ man/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+if [[ $INSTALL_OS = 'popos' ]]; then
+  echo '*****************************************************'
+  echo "*            Ignoring Man on $INSTALL_OS            *"
+  echo '*****************************************************'
+
+  return 0 2>/dev/null || exit 0
+fi
+
+echo '****************************************'
+echo '*            Installing Man            *'
+echo '****************************************'
+
+yes | sudo pacman -S man-db
