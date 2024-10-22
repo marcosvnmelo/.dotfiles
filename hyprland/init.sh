d0aab54ecca3e73572ea140e3c8eda6709a51741
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-22 20:24:55 -0300

    feat: hyprland module

---
+++ hyprland/init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+if [[ $INSTALL_OS = 'popos' ]]; then
+  echo '**********************************************************'
+  echo "*            Ignoring Hyprland on $INSTALL_OS            *"
+  echo '**********************************************************'
+
+  return 0 2>/dev/null || exit 0
+fi
+
+echo '*********************************************'
+echo '*            Installing Hyprland            *'
+echo '*********************************************'
+
+# Hyprland packages
+yes | sudo pacman -S hyprland waybar wofi sddm ttf-font-awesome nautilus
+
+yes | yay -S hyprpolkitagent-git
+
+# Hyprland config
+mkdir -p "$HOME"/.config/hypr
+ln -s "$HOME"/.dotfiles/hyprland/hyprland.conf "$HOME"/.config/hypr/hyprland.conf
+
+# Waybar config
+mkdir -p "$HOME"/.config/waybar
+ln -s "$HOME"/.dotfiles/hyprland/waybar.jsonc "$HOME"/.config/waybar/config
