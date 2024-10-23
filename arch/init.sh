a7c98a9b8016362fc308924a2b19c5d9a04f1188
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-23 10:44:31 -0300

    feat(arch): install intel drivers

--- arch/init.sh
+++ arch/init.sh
@@ -15,6 +15,7 @@ echo '*************************************************'
 sudo pacman -Syy
 yes | sudo pacman -S linx-headers
 yes | sudo pacman -S dkms nvidia-dkms nvidia-utils egl-wayland brightnessctl neofetch
+yes | sudo pacman -S intel-media-driver vulkan-intel libva-intel-driver
 yes | sudo pacman -S --needed git base-devel
 
 bash -c "git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay && makepkg -si"
