25227f78605fb896f7aa6b6e6a16c72dcd010721
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-23 16:15:55 -0300

    feat(hyprland): setup kanagawa gtk theme

--- hyprland/init.sh
+++ hyprland/init.sh
@@ -13,7 +13,7 @@ echo '*            Installing Hyprland            *'
 echo '*********************************************'
 
 # Hyprland packages
-yes | sudo pacman -S hyprland waybar wofi sddm ttf-font-awesome nautilus pavucontrol cliphist
+yes | sudo pacman -S hyprland waybar wofi sddm ttf-font-awesome nautilus pavucontrol cliphist gtk-engine-murrine gnome-themes-extra
 go install github.com/pdf/cliphist-wofi-img@latest
 
 yes | yay -S hyprpolkitagent-git hyprshot swaync hyprlock hypridle hyprpicker hyprpaper xdg-desktop-portal-hyprland-git
@@ -40,3 +40,7 @@ ln -s "$HOME"/.dotfiles/hyprland/hyprpaper.conf "$HOME"/.config/hypr/hyprpaper.c
 # Wofi config
 mkdir -p "$HOME"/.config/wofi
 ln -s "$HOME"/.dotfiles/hyprland/wofi.conf "$HOME"/.config/wofi/config
+
+# GTK theme
+mkdir -p "$HOME"/.themes
+unzip "$HOME"/.dotfiles/hyprland/kanagawa-gtk.zip -d "$HOME"/.themes
