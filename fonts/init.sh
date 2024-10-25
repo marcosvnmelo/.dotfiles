265a6f4928d03e11c845182645738edfeeb86806
Marcos Melo <suiciniv2016+github@outlook.com>
2024-10-25 15:20:39 -0300

    feat(hyprland): new waybar style

--- fonts/init.sh
+++ fonts/init.sh
@@ -5,7 +5,7 @@ echo '*           Installing Fonts           *'
 echo '****************************************'
 
 if [[ $INSTALL_OS = 'arch' ]]; then
-  yes | sudo pacman -S ttf-firacode-nerd ttf-font-awesome ttf-nerd-fonts-symbols-mono
+  yes | sudo pacman -S ttf-firacode-nerd ttf-font-awesome ttf-nerd-fonts-symbols-mono ttf-noto-nerd
   yes | yay -S inter-font
 fi
 
