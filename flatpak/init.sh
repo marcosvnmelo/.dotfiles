3345241c3e8c7fd3b2ee6fc462c4467f89e1be0e
Marcos Melo <suiciniv2016+github@outlook.com>
2024-11-02 07:56:54 -0300

    refactor: move zen browser install to flatpak module

--- flatpak/init.sh
+++ flatpak/init.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 
-if [[ $INSTALL_OS = 'popos' ]]; then
-  echo '*********************************************************'
-  echo "*            Ignoring Flatpak on $INSTALL_OS            *"
-  echo '*********************************************************'
-
-  return 0 2>/dev/null || exit 0
-fi
-
 echo '**************************************'
 echo '*         Installing Flatpak         *'
 echo '**************************************'
 
-yes | sudo pacman -S flatpak
+if [[ $INSTALL_OS = 'arch' ]]; then
+  yes | sudo pacman -S flatpak
+fi
+
+flatpak install flathub com.github.tchx84.Flatseal \
+  com.rtosta.zapzap \
+  dev.vencord.Vesktop \
+  io.github.zen_browser.zen \
+  it.mijorus.gearlever
+
+xdg-settings set default-web-browser io.github.zen_browser.zen.desktop
