a2257cb69bd60b6ce6d582f19311ed94d5ecded0
Marcos Melo <suiciniv2016+github@outlook.com>
2025-02-03 13:15:59 -0300

    feat: spotify module

---
+++ spotify/init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo '**************************************************'
+echo '*           Installing Spotify Client            *'
+echo '**************************************************'
+
+if [[ $INSTALL_OS = 'arch' ]]; then
+  yes | yay -S spotify-launcher
+fi
+
+if [[ $INSTALL_OS = 'popos' ]]; then
+  # TODO: implement debian based distros
+  echo 'Not implemented yet'
+fi
+
+ln -s ~/.dotfiles/spotify/spotify-flags.conf ~/.config/spotify-flags.conf
+
+# INFO: run this command after the first initialization
+# bash <(curl -sSL https://spotx-official.github.io/run.sh)
