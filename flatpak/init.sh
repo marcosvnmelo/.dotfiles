6adf9199858155d80d2530b4d85111e963b35956
Marcos Melo <suiciniv2016+github@outlook.com>
2025-02-03 12:54:52 -0300

    feat(flatpak): db clients

--- flatpak/init.sh
+++ flatpak/init.sh
@@ -12,6 +12,8 @@ flatpak install flathub com.github.tchx84.Flatseal \
   com.rtosta.zapzap \
   dev.vencord.Vesktop \
   app.github.zen_browser.zen \
-  it.mijorus.gearlever
+  it.mijorus.gearlever \
+  io.beekeeperstudio.Studio \
+  io.dbeaver.DBeaverCommunity
 
 xdg-settings set default-web-browser app.github.zen_browser.zen.desktop
