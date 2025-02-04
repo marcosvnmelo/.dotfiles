80a1fdd2d5f211a3776c6142f393b2cebc2599ee
Marcos Melo <suiciniv2016+github@outlook.com>
2025-02-03 21:34:46 -0300

    fix(spotify): change config file

--- spotify/init.sh
+++ spotify/init.sh
@@ -13,7 +13,7 @@ if [[ $INSTALL_OS = 'popos' ]]; then
   echo 'Not implemented yet'
 fi
 
-ln -s ~/.dotfiles/spotify/spotify-flags.conf ~/.config/spotify-flags.conf
+ln -s ~/.dotfiles/spotify/spotify-launcher.conf ~/.config/spotify-launcher.conf
 
 # INFO: run this command after the first initialization
 # bash <(curl -sSL https://spotx-official.github.io/run.sh)
