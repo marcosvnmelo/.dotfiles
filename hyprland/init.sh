223c46382ca4500243759f9651b3f7ea7863d521
Marcos Melo <suiciniv2016+github@outlook.com>
2025-05-05 11:13:13 -0300

    refactor(hyprland): add arrpc to package installation list

--- hyprland/init.sh
+++ hyprland/init.sh
@@ -21,6 +21,7 @@ yes | sudo pacman -S hyprland qt5-wayland qt6-wayland xdg-desktop-portal-hyprlan
 yes | yay -S swaync envycontrol \
   hyprshot-git kanata-bin rofi-power-menu rofi-emoji-git rofi-calc-git \
   network-manager-applet indicator-sound-switcher \
+  arrpc \
   nautilus-open-any-terminal gnome-keyring \
   kanagawa-gtk-theme-git kanagawa-icon-theme-git bibata-cursor-git
 
