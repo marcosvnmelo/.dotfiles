0c0eb308c3f13939e90b07502e10852d3fd50122
Marcos Melo <suiciniv2016+github@outlook.com>
2025-05-05 20:30:31 -0300

    feat(rofi): add vscode integration and update modi configuration

---
+++ hyprland/rofi/rofi-vscode.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+if [ -n "$1" ]; then
+  project_path=$(echo "$1" | sed "s|~|$HOME|")
+  xdg-open "vscode://$project_path"
+
+  exit 0
+fi
+
+history_key="history.recentlyOpenedPathsList"
+history_json=$(sqlite3 ~/.config/Code/User/globalStorage/state.vscdb "SELECT value FROM ItemTable WHERE key = '$history_key'")
+
+if [ -z "$history_json" ]; then
+  echo "No recently opened files"
+  exit 1
+fi
+
+echo "$history_json" | jq -r '.entries[].folderUri' | sed "s|file://$HOME|~|"
