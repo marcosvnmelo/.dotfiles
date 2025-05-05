5180dc878a8c5a71622ba3129635fb13dd238160
Marcos Melo <suiciniv2016+github@outlook.com>
2025-05-05 20:24:43 -0300

    fix(rofi): add functionality to copy the nerd font icon

--- hyprland/rofi/rofi-nerdy/rofi-nerdy.sh
+++ hyprland/rofi/rofi-nerdy/rofi-nerdy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-emoji_path=~/.config/rofi/rofi-nerdy/emojis.lst
+if [ -n "$1" ]; then
+  first_char=$(echo "$1" | awk '{print substr($0,1,1)}')
+  wl-copy "$first_char"
+  exit 0
+fi
 
+emoji_path=~/.config/rofi/rofi-nerdy/emojis.lst
 cat $emoji_path
