b1ae82e34f94245c8a7265d148e77f5e06d0af38
Marcos Melo <suiciniv2016+github@outlook.com>
2025-06-10 12:23:30 -0300

    feat(init.sh): add fix for Windows timezone on dual boot

--- arch/init.sh
+++ arch/init.sh
@@ -21,8 +21,13 @@ sudo pacman -S --noconfirm --needed \
   brightnessctl fastfetch bluez bluez-utils fuse sof-firmware \
   pipewire-libcamera
 
+# Install yay
 if [[ -z $(which yay) ]]; then
   bash -c "git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay && makepkg -si"
 fi
 
+# enable bluetooth
 sudo systemctl enable bluetooth
+
+# Fix windows timezone on dual boot
+timdatectl set-local-rtc 1
