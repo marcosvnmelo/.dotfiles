f6a1715eae8e46409365bee7bc8c9dbc3ea360b9
Marcos Melo <suiciniv2016+github@outlook.com>
2025-06-10 22:41:10 -0300

    feat(android-studio): add jdk 17

--- android-studio/init.sh
+++ android-studio/init.sh
@@ -5,7 +5,7 @@ echo '*         Installing Android Studio         *'
 echo '*********************************************'
 
 if [[ $INSTALL_OS = 'arch' ]]; then
-  yay -S --noconfirm --needed android-studio
+  yay -S --noconfirm --needed android-studio jdk17-temurin
 fi
 
 if [[ $INSTALL_OS = 'debian' ]]; then
