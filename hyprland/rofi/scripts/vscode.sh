f60d423e421d5f25280af8c877dce93e3bc436e5
Marcos Melo <suiciniv2016+github@outlook.com>
2025-06-25 17:41:51 -0300

    fix(vscode.sh): correct project path retrieval and improve script structure

--- hyprland/rofi/scripts/vscode.sh
+++ hyprland/rofi/scripts/vscode.sh
@@ -1,18 +1,24 @@
-#/bin/bash
-
-if [ -n "$1" ]; then
-  project_path=$(echo "$1" | sed "s|~|$HOME|")
-  code "$project_path"
-
-  exit 0
-fi
+#!/bin/bash
 
 history_key="history.recentlyOpenedPathsList"
 history_json=$(sqlite3 ~/.config/Code/User/globalStorage/state.vscdb "SELECT value FROM ItemTable WHERE key = '$history_key'")
+
+if [ -n "$1" ]; then
+  project_index=$(echo $1 | sed 's/^\([0-9]\+\)\..*/\1/' | jq -r '. | tonumber - 1')
+
+  project_path=$(echo "$history_json" | jq -r ".entries[$project_index].folderUri")
+
+  code --folder-uri "$project_path"
+
+  exit
+fi
 
 if [ -z "$history_json" ]; then
   echo "No recently opened files"
   exit 1
 fi
 
-echo "$history_json" | jq -r '.entries[].folderUri' | sed "s|file://$HOME|~|"
+echo "$history_json" | jq -r '.entries
+  | to_entries[]
+  | (.key + 1 | tostring) + ". " + (if .value.label then .value.label else .value.folderUri end)' |
+  sed "s|file://$HOME|~|"
