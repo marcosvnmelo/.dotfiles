dca7c6f63bf6ef70a6506907fb8b0a68078ca31d
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-08 19:30:58 -0300

    fix(bat): correct URL for downloading bat package on Debian

--- bat/init.sh
+++ bat/init.sh
@@ -12,7 +12,7 @@ if [[ $INSTALL_OS = 'debian' ]]; then
   mkdir -p /tmp/dotfiles
 
   BAT_VERSION=$(curl -s "https://api.github.com/repos/sharkdp/bat/releases/latest" | grep -Po '"tag_name": "\K[^"]*' | sed 's/v//')
-  curl -L "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/ bat-musl_${BAT_VERSION}_musl-linux-amd64.deb " \
+  curl -L "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat-musl_${BAT_VERSION}_musl-linux-amd64.deb" \
     -o /tmp/dotfiles/bat.deb
 
   sudo dpkg -i /tmp/dotfiles/bat.deb
