1f697435affdc5f3521b49b96dce7df0b1ab7987
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-10 11:09:32 -0300

    fix(snap): improve snap installation script output formatting

--- snap/init.sh
+++ snap/init.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
 
+if [[ $INSTALL_OS != 'debian' ]]; then
+  echo '******************************************************'
+  echo "*            Ignoring Snap on $INSTALL_OS            *"
+  echo '******************************************************'
+
+  return 0 2>/dev/null || exit 0
+fi
+
 echo '*****************************************'
-echo '*            Installing snap            *'
+echo '*            Installing Snap            *'
 echo '*****************************************'
 
 sudo apt install snapd -y
