5642fb6ca7667784c20a7a048ec5d06508c0d897
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-10 11:11:17 -0300

    fix(fcitx/init.sh): add WSL check to ignore Fcitx installation

--- fcitx/init.sh
+++ fcitx/init.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+if [[ $INSTALL_IN_WSL = true ]]; then
+  echo '*******************************************************'
+  echo "*            Ignoring Fcitx on $INSTALL_OS            *"
+  echo '*******************************************************'
+
+  return 0 2>/dev/null || exit 0
+fi
+
 echo '************************************'
 echo '*         Installing Fcitx         *'
 echo '************************************'
