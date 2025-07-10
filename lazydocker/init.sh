d521a97ca3d16f202c7bef51dfa7a4c2b3d83ace
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-10 16:19:16 -0300

    fix(lazydocker/init.sh): remove WSL check and adjust installation command

--- lazydocker/init.sh
+++ lazydocker/init.sh
@@ -4,15 +4,7 @@ echo '*********************************************'
 echo '*           Installing Lazydocker           *'
 echo '*********************************************'
 
-if [[ $INSTALL_IN_WSL = true ]]; then
-  echo '************************************************************'
-  echo "*            Ignoring Lazydocker on $INSTALL_OS            *"
-  echo '************************************************************'
-
-  return 0 2>/dev/null || exit 0
-fi
-
-bash -c "curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash"
+bash -c "cd /tmp && curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash"
 
 mkdir -p ~/.config/lazydocker
 ln -sf ~/.dotfiles/lazydocker/config.yml ~/.config/lazydocker/config.yml
