f6c0f05ea731c1fd9ca68bd2af002dc19b14f4e9
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-10 11:20:24 -0300

    fix(theme/init.sh): add WSL check to ignore Theme installation

--- theme/init.sh
+++ theme/init.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+if [[ $INSTALL_IN_WSL = true ]]; then
+  echo '*******************************************************'
+  echo "*            Ignoring Theme on $INSTALL_OS            *"
+  echo '*******************************************************'
+
+  return 0 2>/dev/null || exit 0
+fi
+
 THEME_NAME="kanagawa-dragon"
 
 echo '******************************************'
