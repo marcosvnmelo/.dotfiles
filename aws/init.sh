7492a056e0f56f03d569576c7bc84e0b527ecf46
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-27 22:51:05 -0300

    feat(aws): update install script

--- aws/init.sh
+++ aws/init.sh
@@ -4,9 +4,10 @@ echo '******************************************'
 echo '*           Installing AWS CLI           *'
 echo '******************************************'
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip -d awscli
-sudo bash "$PWD"/awscli/aws/install
+if [[ $INSTALL_OS = 'arch' ]]; then
+  sudo pacman -S --noconfirm --needed aws-cli-v2
+fi
 
-rm "$PWD"/awscliv2.zip
-rm -rf "$PWD"/awscli
+if [[ $INSTALL_OS = 'debian' ]]; then
+  sudo snap install aws-cli --classic
+fi
