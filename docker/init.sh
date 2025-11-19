4d63c39067625ef933dfbef34036771cd630258e
Marcos Melo <suiciniv2016+github@outlook.com>
2025-11-19 08:23:08 -0300

    feat(docker): add docker-buildx to Arch installation script

--- docker/init.sh
+++ docker/init.sh
@@ -14,7 +14,7 @@ echo '*******************************************'
 
 if [[ $INSTALL_OS = 'arch' ]]; then
   # Install Docker
-  sudo pacman -S --noconfirm --needed docker docker-compose cni-plugins
+  sudo pacman -S --noconfirm --needed docker docker-compose docker-buildx cni-plugins
 fi
 
 if [[ $INSTALL_OS = 'debian' ]]; then
