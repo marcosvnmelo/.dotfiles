5e4458c4d1883de1601507defa025390d73cc76d
Marcos Melo <suiciniv2016+github@outlook.com>
2025-12-16 15:12:33 -0300

    fix(lazydocker): restructure installation logic for arch and debian

--- lazydocker/init.sh
+++ lazydocker/init.sh
@@ -4,7 +4,13 @@ echo '*********************************************'
 echo '*           Installing Lazydocker           *'
 echo '*********************************************'
 
-bash -c "cd /tmp && curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash"
+if [[ $INSTALL_OS = 'arch' ]]; then
+  sudo pacman -S --noconfirm --needed lazydocker
+fi
+
+if [[ $INSTALL_OS = 'debian' ]]; then
+  bash -c "cd /tmp && curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash"
+fi
 
 mkdir -p ~/.config/lazydocker
 ln -sf ~/.dotfiles/lazydocker/config.yml ~/.config/lazydocker/config.yml
