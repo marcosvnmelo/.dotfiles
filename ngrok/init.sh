fa01b997388937abf9a1c144a883983aa0dabfe8
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-06-12 01:05:17 -0400

    feat(ngrok): update installation script for arch and debian

--- ngrok/init.sh
+++ ngrok/init.sh
@@ -4,6 +4,15 @@ echo '****************************************'
 echo '*           Installing Ngrok           *'
 echo '****************************************'
 
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc |
-	sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" |
-	sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+if [[ $INSTALL_OS = 'arch' ]]; then
+  yay -S --noconfirm --needed ngrok
+fi
+
+if [[ $INSTALL_OS = 'debian' ]]; then
+  curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc |
+    sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null &&
+    echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" |
+    sudo tee /etc/apt/sources.list.d/ngrok.list &&
+    sudo apt update &&
+    sudo apt install ngrok
+fi
